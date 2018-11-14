344bf62768ce604ae96851f7945aabfccd6b890d
dannykuijpers <302146918@student.rocmondriaan.nl>
2018-11-14 14:23:49 +0100

    naar links animatie moet nog even gefixt worden

--- Hero.java
+++ Hero.java
@@ -24,6 +24,17 @@ public class Hero extends Mover {
     private GreenfootImage walk9 = new GreenfootImage("p1_walk09.png");
     private GreenfootImage walk10 = new GreenfootImage("p1_walk10.png");
     private GreenfootImage walk11 = new GreenfootImage("p1_walk11.png");
+    private GreenfootImage walk1mir = new GreenfootImage("p1_walk01_mirrored.png");
+    private GreenfootImage walk2mir = new GreenfootImage("p1_walk02_mirrored.png");
+    private GreenfootImage walk3mir = new GreenfootImage("p1_walk03_mirrored.png");
+    private GreenfootImage walk4mir = new GreenfootImage("p1_walk04_mirrored.png");
+    private GreenfootImage walk5mir = new GreenfootImage("p1_walk05_mirrored.png");
+    private GreenfootImage walk6mir = new GreenfootImage("p1_walk06_mirrored.png");
+    private GreenfootImage walk7mir = new GreenfootImage("p1_walk07_mirrored.png");
+    private GreenfootImage walk8mir = new GreenfootImage("p1_walk08_mirrored.png");
+    private GreenfootImage walk9mir = new GreenfootImage("p1_walk09_mirrored.png");
+    private GreenfootImage walk10mir = new GreenfootImage("p1_walk10_mirrored.png");
+    private GreenfootImage walk11mir = new GreenfootImage("p1_walk11_mirrored.png");
     private int frame = 1;
 
 
@@ -71,16 +82,67 @@ public class Hero extends Mover {
         
         if (Greenfoot.isKeyDown("left")) {
             velocityX = -5;
+            animateleft();
         } else if (Greenfoot.isKeyDown("right")) {
             velocityX = 5;
             animateright();
         }
     }
     
+    public void animateleft(){
+        if(frame == 1)
+        {
+            setImage(walk1mir);
+        }
+        else if(frame == 2)
+        {
+            setImage(walk2mir);
+        }
+        else if(frame == 3)
+        {
+            setImage(walk3mir);
+        }
+        else if(frame == 4)
+        {
+            setImage(walk4mir);
+        }
+        else if(frame == 5)
+        {
+            setImage(walk5mir);
+        }
+        else if(frame == 6)
+        {
+            setImage(walk6mir);
+        }
+        else if(frame == 7)
+        {
+            setImage(walk7mir);
+        }
+        else if(frame == 8)
+        {
+            setImage(walk8mir);
+        }
+        else if(frame == 9)
+        {
+            setImage(walk9mir);
+        }
+        else if(frame == 10)
+        {
+            setImage(walk10mir);
+        }
+        else if(frame == 11)
+        {
+            setImage(walk11mir);
+            frame = 1;
+            return;
+        }
+        frame++;
+    }
+    
     public void animateright()
     {
         if(frame == 1)
-        {
+        { 
             setImage(walk1);
         }
         else if(frame == 2)
